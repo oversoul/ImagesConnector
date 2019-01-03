16835a059759861d2b3f3ba3f0340aa39da8a614
Amine <kurosoul.ae@gmail.com>
2019-01-03 13:06:59 +0100

    fixing exoquant import

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,26 @@
-extern crate image;
-extern crate rayon;
 extern crate exoquant;
+extern crate image;
 extern crate imageproc;
+extern crate rayon;
 
+use exoquant::{convert_to_indexed, ditherer, optimizer, Color as ExoColor};
 use image::Rgba;
-use exoquant::*;
-use rayon::prelude::*;
 use imageproc::drawing::draw_text_mut;
-use rusttype::{ FontCollection, Scale };
-use std::{error::Error, fmt, fs, process, path::Path};
-
+use rayon::prelude::*;
+use rusttype::{FontCollection, Scale};
+use std::{error::Error, fmt, fs, path::Path, process};
 
 #[derive(Debug)]
 struct Color {
     primary: [u8; 4],
-    secondary: [u8; 4]
+    secondary: [u8; 4],
 }
 
 #[derive(Debug)]
 struct Paths {
     first_path: String,
     second_path: String,
-    export_path: String
+    export_path: String,
 }
 
 impl Paths {
@@ -34,7 +33,11 @@ impl Paths {
         let first_path = args.next().unwrap();
         let second_path = args.next().unwrap();
         let export_path = args.next().unwrap();
-        Ok(Paths { first_path, second_path, export_path })
+        Ok(Paths {
+            first_path,
+            second_path,
+            export_path,
+        })
     }
 }
 
@@ -67,7 +70,6 @@ impl From<std::io::Error> for AppError {
 }
 
 fn main() {
-
     let paths = match Paths::new(std::env::args()) {
         Ok(p) => p,
         Err(_) => {
@@ -76,20 +78,31 @@ fn main() {
             process::exit(1);
         }
     };
-    
-    let months: Vec<_> = fs::read_dir(&paths.first_path).unwrap().map(|res| res.unwrap().path()).collect();
-    let images: Vec<_> = fs::read_dir(&paths.second_path).unwrap().map(|res| res.unwrap().path()).collect();    
 
-    let _files: Vec<_> = images.par_iter().map(|image| {
+    let months: Vec<_> = fs::read_dir(&paths.first_path)
+        .unwrap()
+        .map(|res| res.unwrap().path())
+        .collect();
+    let images: Vec<_> = fs::read_dir(&paths.second_path)
+        .unwrap()
+        .map(|res| res.unwrap().path())
+        .collect();
+
+    images.par_iter().for_each(|image| {
         let color = get_color_palette(image.as_path());
-        let _ms: Vec<_> = months.par_iter().map(|month| {
-            // file_stem(), file_name() with extension 
-            let path = format!("{}/{}-{}.png", &paths.export_path, month.file_stem().unwrap().to_str().unwrap(), image.file_stem().unwrap().to_str().unwrap());
+        months.par_iter().for_each(|month| {
+            // file_stem(), file_name() with extension
+            let path = format!(
+                "{}/{}-{}.png",
+                &paths.export_path,
+                month.file_stem().unwrap().to_str().unwrap(),
+                image.file_stem().unwrap().to_str().unwrap()
+            );
             let image_path = Path::new(&path);
             join_photos_vertically(image.as_path(), month.as_path(), image_path).unwrap();
-            write_text(image_path, &color)
-        }).filter_map(|x| Some(x)).collect();
-    }).filter_map(|x| Some(x)).collect();
+            write_text(image_path, &color);
+        });
+    });
 }
 
 fn print_help() {
@@ -105,13 +118,22 @@ fn print_help() {
     println!("export_path:   folder where to save exported images.");
 }
 
-
 fn get_color_palette(path: &Path) -> Color {
     let img = image::open(path).expect("image couldn't be opened!");
     let img = img.to_rgba();
     let (width, _) = img.dimensions();
-    let pixels: Vec<exoquant::Color> = img.into_raw().chunks(4).map(|c| exoquant::Color::new(c[0], c[1], c[2], c[3])).collect();
-    let (palette, _) = convert_to_indexed(&pixels, width as usize, 256, &optimizer::KMeans, &ditherer::FloydSteinberg::new());
+    let pixels: Vec<ExoColor> = img
+        .into_raw()
+        .chunks(4)
+        .map(|c| ExoColor::new(c[0], c[1], c[2], c[3]))
+        .collect();
+    let (palette, _) = convert_to_indexed(
+        &pixels,
+        width as usize,
+        256,
+        &optimizer::KMeans,
+        &ditherer::FloydSteinberg::new(),
+    );
     // making sure always alpha is 255.
     let primary = [palette[0].r, palette[0].g, palette[0].b, 255u8];
     // random index, convert_to_index returns a Vec of len = 256
@@ -119,7 +141,11 @@ fn get_color_palette(path: &Path) -> Color {
     Color { primary, secondary }
 }
 
-fn join_photos_vertically(first_path: &Path, second_path: &Path, result_path: &Path) -> Result<(), AppError> {
+fn join_photos_vertically(
+    first_path: &Path,
+    second_path: &Path,
+    result_path: &Path,
+) -> Result<(), AppError> {
     let first_img = image::open(first_path)?;
     let second_img = image::open(second_path)?;
 
@@ -136,7 +162,7 @@ fn join_photos_vertically(first_path: &Path, second_path: &Path, result_path: &P
     // getting the full width.
     let width = first_size.0;
     // joining up both heights
-    let height = first_size.1 + second_size.1;    
+    let height = first_size.1 + second_size.1;
 
     let mut first_pxs = first_img.into_raw();
     let second_pxs = second_img.into_raw();
@@ -148,7 +174,6 @@ fn join_photos_vertically(first_path: &Path, second_path: &Path, result_path: &P
     image::save_buffer(result_path, buffer, width, height, image::RGBA(8))?;
     Ok(())
 }
-
 
 fn write_text(path: &Path, color: &Color) -> u32 {
     // image path
@@ -162,15 +187,37 @@ fn write_text(path: &Path, color: &Color) -> u32 {
     let font = Vec::from(include_bytes!("JosefinSans-Thin.ttf") as &[u8]);
 
     //  load font.
-    let font = FontCollection::from_bytes(font).unwrap().into_font().unwrap();
+    let font = FontCollection::from_bytes(font)
+        .unwrap()
+        .into_font()
+        .unwrap();
 
     let height = 250.0;
-    let scale = Scale { x: height * 1.0, y: height };
-    draw_text_mut(&mut img, Rgba(color.primary), 380, 580 + 2455, scale, &font, "20");
-    draw_text_mut(&mut img, Rgba(color.secondary), 660, 580 + 2455, scale, &font, "19");
+    let scale = Scale {
+        x: height * 1.0,
+        y: height,
+    };
+    draw_text_mut(
+        &mut img,
+        Rgba(color.primary),
+        380,
+        580 + 2455,
+        scale,
+        &font,
+        "20",
+    );
+    draw_text_mut(
+        &mut img,
+        Rgba(color.secondary),
+        660,
+        580 + 2455,
+        scale,
+        &font,
+        "19",
+    );
 
     match img.save(path) {
         Ok(_) => 1,
-        Err(_) => 0
+        Err(_) => 0,
     }
 }
